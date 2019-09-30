03ca266930573c8ee67c3024cdc179574b40fdf1
lpl0129 <lpl0129@126.com>
2019-09-30 11:53:33 +0800

    极光推送

--- app/src/main/java/com/easyhome/jrconsumer/mvp/ui/activity/WelcomeActivity.kt
+++ app/src/main/java/com/easyhome/jrconsumer/mvp/ui/activity/WelcomeActivity.kt
@@ -1,7 +1,9 @@
 package com.easyhome.jrconsumer.mvp.ui.activity
 
+import android.app.Notification
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 
 import com.jess.arms.base.BaseActivity
 import com.jess.arms.di.component.AppComponent
@@ -16,6 +18,14 @@ import com.easyhome.jrconsumer.R
 import com.easyhome.jrconsumer.app.extension.singleClick
 import kotlinx.android.synthetic.main.activity_welcome.*
 import org.jetbrains.anko.startActivity
+import android.app.NotificationManager
+import android.app.NotificationChannel
+import android.graphics.Color
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.support.v4.app.NotificationCompat
+import android.view.View
+import me.leolin.shortcutbadger.ShortcutBadger
 
 
 /**
@@ -24,6 +34,8 @@ import org.jetbrains.anko.startActivity
  */
 class WelcomeActivity : BaseActivity<WelcomePresenter>(), WelcomeContract.View {
 
+    var sMessageCount = 1
+    lateinit var mNotificationManager: NotificationManager
     override fun setupActivityComponent(appComponent: AppComponent) {
         DaggerWelcomeComponent //如找不到该类,请编译一下项目
             .builder()
@@ -44,6 +56,57 @@ class WelcomeActivity : BaseActivity<WelcomePresenter>(), WelcomeContract.View {
         skipB.singleClick {
             startActivity<MainActivity>()
         }
+        //测试桌面红点
+        mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        timer()
+    }
+
+    fun timer() {
+        val handler = Handler()
+        val runnable = object : Runnable {
+            override fun run() {
+                //要做的事情
+                testNotify()
+            }
+        }
+        handler.postDelayed(runnable, 5000);
+    }
+
+
+    //测试通知
+    fun testNotify() {
+        val builder: NotificationCompat.Builder
+        //8.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            builder = NotificationCompat.Builder(this, getChannelId())
+        } else {
+            builder = NotificationCompat.Builder(this, "456")
+            //8.0以下版本桌面红点显示
+            ShortcutBadger.applyCount(this, ++sMessageCount);
+        }
+
+        builder.setSmallIcon(R.mipmap.launcher)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setTicker("title")
+            .setAutoCancel(true)
+            .setContentTitle("contentTitle")
+            .setContentText("contentText")
+
+        mNotificationManager.notify(123, builder.build())
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    fun getChannelId(): String {
+        val channelId = "1"
+        val channelName = "com.easyhome.jrconsumer"
+
+        val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+        channel.enableLights(true)//显示桌面红点
+        channel.lightColor = Color.RED
+        channel.setShowBadge(true)
+        mNotificationManager.createNotificationChannel(channel)
+        return channel.id
     }
 
     override fun showMessage(message: String) {
---
+++ app/src/main/java/com/easyhome/jrconsumer/mvp/ui/service/PushService.java
@@ -0,0 +1,7 @@
+package com.easyhome.jrconsumer.mvp.ui.service;
+
+import cn.jpush.android.service.JCommonService;
+
+public class PushService extends JCommonService {
+
+}
